e47830a43e3eda72ef8bf53cf52fba405f3c67aa
Vikram Elango <vikrame@gmail.com>
2014-12-02 13:38:52 -0500

    Added apply(), Objects, Functional Proramming

--- moreBasicClasses.scala
+++ moreBasicClasses.scala
@@ -44,3 +44,52 @@ scala> class BMW5 extends Car with Model{
      | }
 defined class BMW5
 
+//Apply
+scala> class Foo {}
+defined class Foo
+
+scala> object FooMaker {
+     | def apply() = new Foo
+     | }
+defined module FooMaker
+
+scala> val foomaker = FooMaker()
+foomaker: Foo = $iwC$$iwC$$iwC$$iwC$Foo@7662f526
+
+//Object holds single instance of a class, used as factories
+//Aka Companion Objects
+
+scala> object Timer {
+     | var count =0
+     | def getcounter(): Long = {
+     | count += 1
+     | count
+     | }
+     | }
+defined module Timer
+
+scala> Timer.getcounter()
+res0: Long = 1
+
+//Object-Functional Programming
+//Function is a set of traits
+scala> object addOne extends Function1[Int, Int] {
+     | def apply(m: Int) = m+1
+     | }
+defined module addOne
+
+scala> addOne(5)
+res1: Int = 6
+
+scala> class AddOne extends Function1[Int, Int] {
+     | def apply(m: Int) : Int = m + 1
+     | }
+defined class AddOne
+
+scala> val plusOne = new AddOne()
+plusOne: AddOne = <function1>
+
+scala> plusOne(10)
+res0: Int = 11
+
+
